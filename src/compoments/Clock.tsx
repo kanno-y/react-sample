90b663d868150dff6f48781a96422eaffc9615ee
kanno-y <kanno0521@gmail.com>
2022-07-31 14:19:59 +0900

    useLayoutEffect

--- src/compoments/Clock.tsx
+++ src/compoments/Clock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
 // タイマーが呼び出される周期を１秒にする
 const UPDATE_CYCLE = 1000;
@@ -41,7 +41,7 @@ export const Clock = () => {
   }, []);
 
   // localstorageから値を読み込むための副作用
-  useEffect(() => {
+  useLayoutEffect(() => {
     const savedLocale = localStorage.getItem(KEY_LOCALE);
     if (savedLocale !== null) {
       setLocale(getLocaleFromString(savedLocale));
